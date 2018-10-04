c722828511caf821bf8913eb54a95f8215698413
Steven Lin <slin@merchantadvancecap.com>
2018-10-04 12:21:19 -0700

    Add infrastructure folder

---
+++ DDDEf/Infrastructure/Config/OrderStatusEntityConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using DDDEf.Domain.Model.OrderAggregate;
+
+namespace DDDEf.Infrastructure.Config
+{
+    public class OrderStatusEntityConfig : IEntityTypeConfiguration<OrderStatus>
+    {
+        public void Configure(EntityTypeBuilder<OrderStatus> orderStatusConfiguration)
+        {
+            orderStatusConfiguration.ToTable("orderstatus", OrderingContext.DEFAULT_SCHEMA);
+
+            orderStatusConfiguration.HasKey(o => o.Id);
+
+            orderStatusConfiguration.Property(o => o.Id)
+                .HasDefaultValue(1)
+                .ValueGeneratedNever()
+                .IsRequired();
+
+            orderStatusConfiguration.Property(o => o.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+   
+   
+}
---
+++ DDDEf/Infrastructure/OrderingContext.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using DDDEf.Domain.SeedWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Threading;
+using DDDEf.Domain.Model.OrderAggregate;
+using DDDEf.Infrastructure.Config;
+
+
+namespace DDDEf.Infrastructure
+{
+    public class OrderingContext
+        : DbContext, IUnitOfWork
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }       
+        public DbSet<OrderStatus> OrderStatus { get; set; }      
+
+        public OrderingContext(DbContextOptions<OrderingContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderStatusEntityConfig());
+           
+        }
+
+        public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Dispatch Domain Events collection. 
+            // Choices:
+            // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
+            // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
+            // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
+            // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
+            //await _mediator.DispatchDomainEventsAsync(this);
+
+            // After executing this line all the changes (from the Command Handler and Domain Event Handlers) 
+            // performed through the DbContext will be committed
+            var result = await base.SaveChangesAsync();
+
+            return true;
+        }
+    }
+
+    public class OrderingContextDesignFactory : IDesignTimeDbContextFactory<OrderingContext>
+    {
+        public OrderingContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<OrderingContext>()
+                .UseSqlServer("Server=.;Initial Catalog=OrderingDb;Integrated Security=true");
+
+            return new OrderingContext(optionsBuilder.Options);
+        }
+
+       
+    }
+}
