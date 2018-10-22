56987051e4f061686e79d5114d522e82ffea56c8
Steven Lin <slin@merchantadvancecap.com>
2018-10-22 11:10:40 -0700

    Add IRabbitMQPersistentConnection interface

---
+++ DDDRabbitMQ/MQ/DefaultConnection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace DDDRabbitMQ.MQ
+{
+    public class DefaultConnection : IRabbitMQPersistentConnection
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private readonly ILogger<DefaultConnection> _logger;
+        IConnection _connection;
+        bool _disposed;
+
+        public DefaultConnection(IConnectionFactory connectionFactory,
+            ILogger<DefaultConnection> logger)
+        {
+            
+
+        }
+
+
+        public bool IsConnected => throw new NotImplementedException();
+
+        public IModel CreateModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryConnect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DDDRabbitMQ/MQ/IRabbitMQPersistentConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RabbitMQ.Client;
+
+
+namespace DDDRabbitMQ
+{
+    public interface IRabbitMQPersistentConnection : IDisposable
+    {
+        bool IsConnected { get; }
+        bool TryConnect();
+        IModel CreateModel();   
+    }
+}
