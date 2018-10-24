c5c30a47c601d0cbee5ef35ec9d43c40da988fc6
Steven Lin <slin@merchantadvancecap.com>
2018-10-24 11:16:47 -0700

    Implement TryConnect method.

--- DDDRabbitMQ/MQ/DefaultConnection.cs
+++ DDDRabbitMQ/MQ/DefaultConnection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 
@@ -10,30 +11,105 @@ namespace DDDRabbitMQ.MQ
         private readonly ILogger<DefaultConnection> _logger;
         IConnection _connection;
         bool _disposed;
+        object sync_root = new object();
 
         public DefaultConnection(IConnectionFactory connectionFactory,
             ILogger<DefaultConnection> logger)
         {
+            _connectionFactory = connectionFactory ?? throw new ArgumentException(nameof(connectionFactory));
+            _logger = logger ?? throw new ArgumentException(nameof(logger));
             
-
         }
 
 
-        public bool IsConnected => throw new NotImplementedException();
+        public bool IsConnected
+        {
+            get
+            {
+                return _connection != null && _connection.IsOpen && !_disposed;
+            }
+        }
 
         public IModel CreateModel()
         {
-            throw new NotImplementedException();
+            if (!IsConnected)
+            {
+                throw new InvalidOperationException("No connections available to perform CreateModel action");
+            }
+            return _connection.CreateModel();
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            if (_disposed) return;
+            _disposed = true;
+            try
+            {
+                _connection.Dispose();
+            }
+            catch (IOException ex) //Why only interested in catching IO exception?
+            {
+                _logger.LogCritical(ex.ToString());
+            }
         }
 
         public bool TryConnect()
         {
-            throw new NotImplementedException();
+            _logger.LogInformation("RabbitMQ Client is trying to connect...");
+            lock (sync_root)
+            {
+               try
+               {
+                    _connection = _connectionFactory.CreateConnection();
+
+                    if (IsConnected)
+                    {
+                        _connection.ConnectionShutdown += _connection_ConnectionShutdown;
+                        _connection.CallbackException += _connection_CallbackException;
+                        _connection.ConnectionBlocked += _connection_ConnectionBlocked;
+                        _logger.LogInformation
+                            ($"RabbitMQ persistent connection acquired a connection {_connection.Endpoint.HostName} and is subscribed to failure events");
+                        return true;
+                    }
+                    else
+                        return false;
+               }
+               catch (Exception ex)
+               {
+                    _logger.LogCritical
+                        ($"FATAL ERROR: RabbitMQ connections could not be created and opened. {ex.ToString()}");
+                    return false;
+               }
+
+            }
+           
+        }
+
+        private void _connection_ConnectionBlocked(object sender, RabbitMQ.Client.Events.ConnectionBlockedEventArgs e)
+        {
+            if (_disposed) return;
+
+            _logger.LogWarning("A RabbitMQ connection is shutdown. Trying to re-connect...");
+
+            TryConnect();
+        }
+
+        private void _connection_CallbackException(object sender, RabbitMQ.Client.Events.CallbackExceptionEventArgs e)
+        {
+            if (_disposed) return;
+
+            _logger.LogWarning("A RabbitMQ connection throw exception. Trying to re-connect...");
+
+            TryConnect();
+        }
+
+        private void _connection_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+            if (_disposed) return;
+
+            _logger.LogWarning("A RabbitMQ connection is on shutdown. Trying to re-connect...");
+
+            TryConnect();
         }
     }
 }
