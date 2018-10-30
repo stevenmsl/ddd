0e5787c8b3162608bbcaf66489aec39f13795c08
Steven Lin <slin@merchantadvancecap.com>
2018-10-30 12:21:47 -0700

    Add DDDEventBus project

---
+++ DDDEventBus/Abstractions/IEventBus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DDDEventBus.Events;
+
+namespace DDDEventBus.Abstractions
+{
+    public interface IEventBus
+    {
+        void Publish(IntegrationEvent @event);
+        void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+        void Unsubscribe<T,TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>;
+        void SubscribeDynamic<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+        void UnsubscribeDynamic<TH>(string eventName)
+            where TH : IDynamicIntegrationEventHandler;
+
+    }
+}
---
+++ DDDEventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using DDDEventBus.Events;
+
+namespace DDDEventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+    public interface IIntegrationEventHandler
+    {
+    }
+}
