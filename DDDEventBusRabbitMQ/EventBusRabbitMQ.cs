9f2ce2b3b3b1161cfc7650caa7b69da92e41bb8d
Steven Lin <slin@merchantadvancecap.com>
2018-11-13 16:52:08 -0800

    Continue implementing EventBusRabbitMQ

--- DDDEventBusRabbitMQ/EventBusRabbitMQ.cs
+++ DDDEventBusRabbitMQ/EventBusRabbitMQ.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text;
 using DDDEventBus;
@@ -8,6 +9,10 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.Client.Exceptions;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Autofac;
+using Microsoft.CSharp;
 
 
 namespace DDDEventBusRabbitMQ
@@ -15,19 +20,24 @@ namespace DDDEventBusRabbitMQ
     public class EventBusRabbitMQ : IEventBus, IDisposable
     {
         const string BROKER_NAME = "ddd_event_bus";
+        const string AUTOFACT_SCOPE_NAME = "ddd_event_bus";
         private readonly IRabbitMQPersistentConnection _persistentConnection;
         private readonly ILogger<EventBusRabbitMQ> _logger;
         private readonly IEventBusSubscriptionsManager _subsManager;
+        private readonly ILifetimeScope _autofac;
+
         private IModel _consumerChannel;
         private string _queueName;
 
         public EventBusRabbitMQ(IRabbitMQPersistentConnection persistentConnection, ILogger<EventBusRabbitMQ> logger,
+            ILifetimeScope autofac,
             IEventBusSubscriptionsManager subsManager, string queueName = null)
         {
             _persistentConnection = persistentConnection;
             _logger = logger ?? throw new ArgumentException(nameof(logger));
             _subsManager = subsManager ?? new InMemoryEventBusSubscriptionsManager();
             _queueName = queueName;
+            _autofac = autofac;
             _consumerChannel = createConsumerChannel();
             _subsManager.OnEventRemoved += subsManager_OnEventRemoved;
 
@@ -70,36 +80,109 @@ namespace DDDEventBusRabbitMQ
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            if (_consumerChannel != null)
+            {
+                _consumerChannel.Dispose();
+            }
+            _subsManager.Clear();
         }
 
         public void Publish(IntegrationEvent @event)
         {
-            throw new NotImplementedException();
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
+
+            using (var channel = _persistentConnection.CreateModel())
+            {
+                var eventName = @event.GetType().Name;
+                channel.ExchangeDeclare(exchange: BROKER_NAME, type: "direct");
+
+                var message = JsonConvert.SerializeObject(@event);
+                var body = Encoding.UTF8.GetBytes(message);
+                var properties = channel.CreateBasicProperties();
+                properties.DeliveryMode = 2; //persistent
+
+                channel.BasicPublish(exchange: BROKER_NAME,
+                    routingKey: eventName,
+                    mandatory: true,
+                    basicProperties: properties,
+                    body: body
+                    );
+            }
+
         }
 
         public void Subscribe<T, TH>()
             where T : IntegrationEvent
             where TH : IIntegrationEventHandler<T>
         {
-            throw new NotImplementedException();
+            var eventName = _subsManager.GetEventKey<T>();
+            doInternalSubscription(eventName);
+            _subsManager.AddSubscription<T, TH>();
         }
 
         public void SubscribeDynamic<TH>(string eventName) where TH : IDynamicIntegrationEventHandler
         {
-            throw new NotImplementedException();
+            doInternalSubscription(eventName);
+            _subsManager.AddDynamicSubscription<TH>(eventName);
         }
 
         public void Unsubscribe<T, TH>()
             where T : IntegrationEvent
             where TH : IIntegrationEventHandler<T>
         {
-            throw new NotImplementedException();
+            _subsManager.RemoveSubscription<T, TH>();
         }
 
         public void UnsubscribeDynamic<TH>(string eventName) where TH : IDynamicIntegrationEventHandler
         {
-            throw new NotImplementedException();
+            _subsManager.RemoveDynamicSubscription<TH>(eventName);
+        }
+
+        private void doInternalSubscription(string eventName)
+        {
+            if (!_subsManager.HasSubscriptionsForEvent(eventName))
+            {
+                using (var channel = _persistentConnection.CreateModel())
+                {
+                    channel.QueueBind(
+                        queue: _queueName,
+                        exchange: BROKER_NAME,
+                        routingKey: eventName
+                    );
+                }
+            }
+        }
+
+        private async Task processEvent(string eventName, string message)
+        {
+            if (_subsManager.HasSubscriptionsForEvent(eventName))
+            {
+                using (var scope = _autofac.BeginLifetimeScope(AUTOFACT_SCOPE_NAME))
+                {
+                    var subs = _subsManager.GetHandlersForEvent(eventName);
+                    foreach (var sub in subs)
+                    {
+                        if (sub.IsDynamic)
+                        {
+                            var handler = scope.ResolveOptional(sub.HandlerType) as IDynamicIntegrationEventHandler;
+                            dynamic eventData = JObject.Parse(message);
+                            await handler.Handle(eventData);
+                        }
+                        else
+                        {
+                            var eventType = _subsManager.GetEventTypeByName(eventName);
+                            var integrationEvent = JsonConvert.DeserializeObject(message, eventType);
+                            var handler = scope.ResolveOptional(sub.HandlerType);
+                            var concreteType = typeof(IIntegrationEventHandler<>).MakeGenericType(eventType);
+                            await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { integrationEvent });
+                        }
+                    }
+                }
+            
+            }
         }
     }
 }
