fc25ef87c49991f5f32146fb08f0d023819675e0
Steven Lin <slin@merchantadvancecap.com>
2018-11-22 13:36:27 -0800

    Add LoanAppliedIntegrationEvent

---
+++ DDDRabbitMQ/IntegrationEvents/Events/LoanAppliedIntegrationEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DDDEventBus.Events;
+
+namespace DDDRabbitMQ.IntegrationEvents.Events
+{
+    public class LoanAppliedIntegrationEvent : IntegrationEvent
+    {
+        public string LoanId { get; set;}
+
+        public LoanAppliedIntegrationEvent(string loanId) => LoanId = loanId;
+
+    }
+}
