1f2e50219df8b9979d719dc7e659e67a8e36732c
Steven Lin <slin@merchantadvancecap.com>
2018-11-26 10:53:58 -0800

    Add Publisher Controller

---
+++ DDDRabbitMQ/Controllers/PublisherController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using DDDEventBus.Abstractions;
+using DDDIEPublisher.IntegrationEvents.Events;
+
+
+namespace DDDIEPublisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+        private IEventBus _eventBus; 
+
+        public PublisherController(IEventBus eventBus)
+        {
+            _eventBus = eventBus;
+        }
+
+        [HttpGet("publish/{command}")]
+        public ActionResult<string> Publish(string command)
+        {
+            var eventMessage = new LoanAppliedIntegrationEvent(Guid.NewGuid().ToString());
+
+            _eventBus.Publish(eventMessage);
+
+            return "published";
+        }
+
+    }
+}
